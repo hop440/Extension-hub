bb297209eafcbc67d362e0ab488c7a7ad610e82b
hop440 <81469542+hop440@users.noreply.github.com>
2024-07-25 14:56:19 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -15,24 +15,14 @@ namespace custom {
      */
     //% block
     export function Print(text: string, line: Line): void {
-
-        let len = 0
-        serial.setBaudRate(9600)
-        serial.redirectToUSB()
-
-        basic.showNumber(Math.trunc((16 - text.length) / 2))
         for (let index = 0; index < Math.trunc((16 - text.length) / 2); index++) {
             text = " " + text
             len += 1
         }
-        if (line) {
+        if (line == Line.Top) {
             Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, text)
         } else {
             Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, text)
         }
-        serial.writeLine(convertToText(line))
-        serial.writeLine(text)
-        serial.writeLine(convertToText(len))
-        serial.writeLine(convertToText(Math.trunc((16 - text.length) / 2)))
     }
 }
