467430d0c2efbd2c46498402c41d6c151bb27b74
hop440 <81469542+hop440@users.noreply.github.com>
2024-07-25 15:32:02 +0200

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -14,16 +14,18 @@ namespace custom {
      * Calculates the space needed to center the text and prints it to the lcd
      */
     //% block
-    export function Print(text: string, line: Line): void {
+    export function Print(text: any, line: Line): void {
+        let str
+        text = convertToText(text)
         for (let index = 0; index < Math.trunc((16 - text.length) / 2); index++) {
-            string = " " + string
+            str = " " + str
         }
-        string + text
+        str + text
         if (line == Line.Top) {
-            Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, string)
+            Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Top, str)
         }
         else {
-            Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, string)
+            Kitronik_VIEWTEXT32.displaySingleLineString(Kitronik_VIEWTEXT32.DisplayLine.Bottom, str)
         }
     }
 }
